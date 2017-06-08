4e7e501d0d58d3ed9e3ca4c1884917811ae59ee7
Hugo <HugoCastelaniBP@gmail.com>
2017-06-08 14:11:40 -0300

    Number generator for numbers page

---
+++ php/numberGenerator.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: hugo
+ * Date: 08/06/17
+ * Time: 14:06
+ */
+
+printf("+%d%d (%d%d) 9%d%d%d%d-%d%d%d%d" , rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9),
+    rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9), rand(1, 9));
