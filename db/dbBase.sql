49bcd69c9df4d5543573b82c1500b179dd3574a5
Hugo <HugoCastelaniBP@gmail.com>
2017-06-23 14:19:54 -0300

    Database redesigned and fully converted to MySQL

--- db/dbBase.sql
+++ db/dbBase.sql
@@ -1,121 +1,133 @@
 /* RELATED TABLES */
 
 /* create user table */
-create table User (
-  id integer not null,
+create table user (
+  id integer not null auto_increment,
   name varchar(50) not null,
   primary key(id));
 
 /* create friendship table */
-create table Friendship (
-  id integer not null,
+create table friendship (
+  id integer not null auto_increment,
   idUserAdded integer not null,
   idUserAdding integer not null,
   primary key(id),
-  foreign key(idUserAdded) references tbUser (id),
-  foreign key(idUserAdding) references tbUser (id));
+  foreign key(idUserAdded) references user (id),
+  foreign key(idUserAdding) references user (id));
 
 /* creating country table */
-create table Country (
-  id integer not null,
-  cdCountry varchar(10) not null,
-  nmCountry varchar(50) not null,
+create table country (
+  id integer not null auto_increment,
+  code varchar(10) not null,
+  name varchar(50) not null,
   primary key(id));
 
 /* creating area table */
-create table Area (
-  id integer not null,
-  cdArea varchar(10) not null,
-  nmArea varchar(50) not null,
+create table area (
+  id integer not null auto_increment,
+  code varchar(10) not null,
+  name varchar(50) not null,
   primary key(id));
 
 /* creating phone table */
-create table Phone (
-  idPhone integer primary key not null,
-  nbPhone varchar(20) not null,
-  idCountryCode integer not null,
-  idAreaCode integer not null,
+create table phone (
+  id integer not null auto_increment,
+  number varchar(20) not null,
+  idCountry integer not null,
+  idArea integer not null,
   idUser integer,
-  foreign key(idCountryCode) references tbCountryCode(idCountryCode),
-  foreign key(idAreaCode) references tbAreaCode(idAreaCode),
-  foreign key(idUser) references tbUser(idUser));
+  primary key(id),
+  foreign key(idCountry) references country(id),
+  foreign key(idArea) references area(id),
+  foreign key(idUser) references user(id));
 
 /* creating notification table */
-create table tbNotification (
-  idNotification integer primary key not null,
+create table notification (
+  id integer not null auto_increment,
   idPhone integer not null,
   idUserNotified integer not null,
-  foreign key(idPhone) references tbPhone(idPhone),
-  foreign key(idUserNotified) references tbUser(idUserNotified));
+  primary key(id),
+  foreign key(idPhone) references phone(id),
+  foreign key(idUserNotified) references user(id));
 
 /* creating denunciation table */
-create table tbDenunciation (
-  idDenunciation integer primary key not null,
+create table denunciation (
+  id integer not null auto_increment,
   idPhone integer not null,
-  foreign key(idPhone) references tbPhone(idPhone));
+  primary key(id),
+  foreign key(idPhone) references phone(id));
 
 /* creating description table */
-create table tbDescription (
-  idDescription integer primary key not null,
-  description varchar(50) not null);
+create table description (
+  id integer not null auto_increment,
+  description varchar(50) not null,
+  primary key(id));
 
 /* creating denunciation/description table */
-create table tbDenunciationDescription (
+create table denunciation_description (
   idDenunciation integer not null,
   idDescription integer not null,
   primary key(idDenunciation, idDescription),
-  foreign key(idDenunciation) references tbDenunciation(idDenunciation),
-  foreign key(idDescription) references tbDescription(idDescription));
+  foreign key(idDenunciation) references denunciation(id),
+  foreign key(idDescription) references description(id));
 
 /* creating suspicious treatment table */
-create table tbSuspiciousTreatment (
-  idSuspiciousTreatment integer primary key not null,
-  nmSuspiciousTreatment varchar(20) not null);
+create table suspicious_treatment (
+  id integer not null auto_increment,
+  name varchar(20) not null,
+  primary key(id));
 
 /* creating treatment configuration table */
-create table tbConfigTreatment (
+create table config_treatment (
   idDescription integer not null,
   idSuspiciousTreatment integer not null,
   primary key(idDescription, idSuspiciousTreatment),
-  foreign key(idDescription) references tbDescription(idDescription),
-  foreign key(idSuspiciousTreatment) references tbSuspiciousTreatment(idSuspiciousTreatment));
+  foreign key(idDescription) references description(id),
+  foreign key(idSuspiciousTreatment) references suspicious_treatment(id));
 
 /* NON-RELATED TABLES
 
 /* creating feedback configuration table */
-create table tbConfigFeedback (
-  nmCallKind varchar(20) primary key not null,
-  showFeedback integer not null);
+create table config_feedback (
+  nmCallKind varchar(20) not null,
+  showFeedback integer not null,
+  primary key(nmCallKind));
 
 /* creating service blocking configuration table */
-create table tbConfigServiceBlock (
-  nmService varchar(20) primary key not null,
-  block integer not null);
+create table config_service_block (
+  nmService varchar(20) not null,
+  block integer not null,
+  primary key(nmService));
 
 /* creating guide configuration table */
-create table tbConfigGuide (
-  nmView varchar(20) primary key not null,
-  showGuide integer not null);
+create table config_guide (
+  nmView varchar(20) not null,
+  showGuide integer not null,
+  primary key(nmView));
 
 /* creating intro configuration table */
-create table tbConfigIntro (
-  showIntro integer primary key not null);
+create table config_intro (
+  showIntro integer not null,
+  primary key(showIntro));
 
-insert into tbConfigIntro values(1);
+insert into config_intro values(1);
 
 /* creating network to download DB configuration table */
-create table tbConfigNetworkDownloadDB (
-  nmNetworkDownload varchar(20) primary key not null,
-  downloadIt integer not null);
+create table config_network_download_db (
+  nmNetwork varchar(20) not null,
+  downloadIt integer not null,
+  primary key(nmNetwork));
 
 /* creating suspicious denunciations amount configuration table */
-create table tbSuspiciousDenunciationsAmount (
-  denunciationAmount integer primary key not null);
+create table suspicious_denunciations_amount (
+  amount integer not null,
+  primary key(amount));
 
 /* creating theme configuration table */
-create table tbConfigTheme (
-  isDark integer primary key not null,
+create table config_theme (
+  isDark integer not null,
   accentColor char(7) not null,
-  defaultAccentColor char(7) not null);
+  defaultAccentColor char(7) not null,
+  primary key(isDark));
 
-insert into tbConfigTheme values(0, '#00BCD4', '#00BCD4');
+insert into config_theme values(0, '#00BCD4', '#00BCD4');
